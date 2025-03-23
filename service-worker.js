ba2209d4035f0bf75983b0ee1a699bbdcb3ca779
Coolio <popgrass25@gmail.com>
2025-03-23 13:49:30 -0500

    ett

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,21 @@
 self.addEventListener('install', (event) => {
-  console.log('Service Worker installed');
+    console.log('Service Worker installed');
+    event.waitUntil(
+        caches.open('coolio-cache').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/css/index.css',
+                '/textures/icons/logo.png'
+            ]);
+        })
+    );
 });
 
 self.addEventListener('fetch', (event) => {
-  // Can be used to cache resources
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
 });
