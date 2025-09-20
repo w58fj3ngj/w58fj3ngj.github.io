7f9eea6ebdaed22f1957cc6e396fb60b60483c5e
Coolio <156616687+w58fj3ngj@users.noreply.github.com>
2025-09-20 16:55:51 -0500

    Update uv.sw.js

--- public/uv.sw.js
+++ public/uv.sw.js
@@ -1,6 +1,6 @@
-importScripts('uv/uv.bundle.js');
-importScripts('uv/uv.config.js');
-importScripts(__uv$config.sw || 'uv/uv.sw.js');
+importScripts('/public/uv/uv.bundle.js');
+importScripts('/public/uv/uv.config.js');
+importScripts(__uv$config.sw || '/public/uv/uv.sw.js');
 
 const uv = new UVServiceWorker();
 
@@ -14,4 +14,5 @@ self.addEventListener("fetch", (event) => {
     })()
   );
 });
+
 
