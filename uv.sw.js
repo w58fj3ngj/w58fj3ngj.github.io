ca3853b9b54e5d93cfaa88af595a372cae209cae
Coolio <156616687+w58fj3ngj@users.noreply.github.com>
2025-09-27 13:06:50 -0500

    a

---
+++ uv.sw.js
@@ -0,0 +1,18 @@
+importScripts('/public/uv/uv.bundle.js');
+importScripts('/public/uv/uv.config.js');
+importScripts(__uv$config.sw || '/public/uv/uv.sw.js');
+
+const uv = new UVServiceWorker();
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    (async () => {
+      if (event.request.url.startsWith(location.origin + __uv$config.prefix)) {
+        return await uv.fetch(event);
+      }
+      return await fetch(event.request);
+    })()
+  );
+});
+
+
