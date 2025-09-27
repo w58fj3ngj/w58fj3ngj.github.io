25c6f4702e2a1d7bd4ecd99f4469490e9d6c0a94
Coolio <156616687+w58fj3ngj@users.noreply.github.com>
2025-09-27 14:16:51 -0500

    e

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -2,7 +2,7 @@
 
 self.__uv$config = {
   prefix: "/service/uv/",
-  bare: "/bare/",
+  bare: "https://i-thought-i-could-trust-you-no-diddy-dog.satucat.uk/tspmo/",
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
   handler: "uv/uv.handler.js",
